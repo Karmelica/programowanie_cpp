654c6a52e3f1f17f2b53aaab7b494894f165cf68
Damian Niedziółka <damianniedziolka7@gmail.com>
2026-05-08 19:20:39 +0200

    5.7

--- Source/CppProgramingEditor.Target.cs
+++ Source/CppProgramingEditor.Target.cs
@@ -8,7 +8,7 @@ public class CppProgramingEditorTarget : TargetRules
 	public CppProgramingEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("CppPrograming");
 	}
